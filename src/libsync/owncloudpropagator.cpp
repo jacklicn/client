99e9aa58117f8fee4231748b57dd2ba2abfa709b
Piotr Mrowczynski <mrow4a@yahoo.com>
2017-08-11 00:03:03 +0200

    Fix paused sync file move issue #5949
    Dont abort final chunk immedietally
    
    Use sync and async aborts

--- src/libsync/owncloudpropagator.cpp
+++ src/libsync/owncloudpropagator.cpp
@@ -734,6 +734,16 @@ PropagatorJob::JobParallelism PropagatorCompositeJob::parallelism()
     return FullParallelism;
 }
 
+void PropagatorCompositeJob::slotSubJobAbortFinished()
+{
+    // Count that job has been finished
+    _abortsCount--;
+
+    // Emit abort if last job has been aborted
+    if (_abortsCount == 0) {
+        emit abortFinished();
+    }
+}
 
 bool PropagatorCompositeJob::scheduleSelfOrChild()
 {
@@ -891,7 +901,8 @@ void PropagateDirectory::slotFirstJobFinished(SyncFileItem::Status status)
 
     if (status != SyncFileItem::Success && status != SyncFileItem::Restoration) {
         if (_state != Finished) {
-            abort();
+            // Synchronously abort
+            abort(AbortType::Synchronous);
             _state = Finished;
             emit finished(status);
         }
