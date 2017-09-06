5d5db2556ed8c2c0fb590a1a62a6d185448ab5a0
Jocelyn Turcotte <jturcotte@woboq.com>
2017-09-06 11:55:37 +0200

    SyncJournalDB: Bind phash as a qlonglong instead of a QString
    This is what csync does and we don't need to pass it as a string
    to let sqlite convert it back to a uint64 later.

--- src/common/syncjournaldb.cpp
+++ src/common/syncjournaldb.cpp
@@ -915,7 +915,7 @@ bool SyncJournalDb::setFileRecord(const SyncJournalFileRecord &_record)
         parseChecksumHeader(record._checksumHeader, &checksumType, &checksum);
         int contentChecksumTypeId = mapChecksumType(checksumType);
         _setFileRecordQuery->reset_and_clear_bindings();
-        _setFileRecordQuery->bindValue(1, QString::number(phash));
+        _setFileRecordQuery->bindValue(1, phash);
         _setFileRecordQuery->bindValue(2, plen);
         _setFileRecordQuery->bindValue(3, record._path);
         _setFileRecordQuery->bindValue(4, record._inode);
@@ -953,7 +953,7 @@ bool SyncJournalDb::deleteFileRecord(const QString &filename, bool recursively)
 
         qlonglong phash = getPHash(filename);
         _deleteFileRecordPhash->reset_and_clear_bindings();
-        _deleteFileRecordPhash->bindValue(1, QString::number(phash));
+        _deleteFileRecordPhash->bindValue(1, phash);
 
         if (!_deleteFileRecordPhash->exec()) {
             return false;
@@ -983,7 +983,7 @@ SyncJournalFileRecord SyncJournalDb::getFileRecord(const QString &filename)
 
     if (!filename.isEmpty() && checkConnect()) {
         _getFileRecordQuery->reset_and_clear_bindings();
-        _getFileRecordQuery->bindValue(1, QString::number(phash));
+        _getFileRecordQuery->bindValue(1, phash);
 
         if (!_getFileRecordQuery->exec()) {
             locker.unlock();
@@ -1110,7 +1110,7 @@ bool SyncJournalDb::updateFileRecordChecksum(const QString &filename,
     auto &query = _setFileRecordChecksumQuery;
 
     query->reset_and_clear_bindings();
-    query->bindValue(1, QString::number(phash));
+    query->bindValue(1, phash);
     query->bindValue(2, contentChecksum);
     query->bindValue(3, checksumTypeId);
 
@@ -1138,7 +1138,7 @@ bool SyncJournalDb::updateLocalMetadata(const QString &filename,
     auto &query = _setFileRecordLocalMetadataQuery;
 
     query->reset_and_clear_bindings();
-    query->bindValue(1, QString::number(phash));
+    query->bindValue(1, phash);
     query->bindValue(2, inode);
     query->bindValue(3, modtime);
     query->bindValue(4, size);
